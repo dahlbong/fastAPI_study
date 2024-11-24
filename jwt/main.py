07eeb54fc14049839437da4dc484a15224dff81d
dahyun <halover111@gmail.com>
2024-11-24 19:14:41 +0900

    menu 라우터 추가

--- jwt/main.py
+++ jwt/main.py
@@ -6,11 +6,13 @@ from starlette.middleware.authentication import AuthenticationMiddleware
 from core.security import JWTAuth
 from users.routes import router as guest_router, user_router
 from auth.route import router as auth_router
+from menus.routes import router as menu_router
 
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(guest_router)
 app.include_router(auth_router)
+app.include_router(menu_router)
 
 #add Middleware
 app.add_middleware(AuthenticationMiddleware, backend=JWTAuth())
